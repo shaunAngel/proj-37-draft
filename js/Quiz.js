4f5975d0091d1227a25ea3ef1a9b62d5147be09f
shaunAngel <76162760+shaunAngel@users.noreply.github.com>
2021-04-26 18:47:35 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,63 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+     question.hide();
+     backgroung("yellow");
+    fill(0)
+    textSize(13)
+    text("result of the quiz,340,50");
+    text("-----", 320, 65)
+    Contestant.getPlayerinfo();
+    if(allContestants !== undefined){
+      var display_answers = 230;
+      fill("blue");
+      textSize(20);
+    text("note:contestanr who answered correct are highlighted in green color!", 130,230);
+    
+    }
+    
+    //write code to change the background color here
+
+    //write code to show a heading for showing the result of Quiz
+
+    //call getContestantInfo( ) here
+
+
+    //write condition to check if contestantInfor is not undefined
+
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+    
+  }
+
+}
